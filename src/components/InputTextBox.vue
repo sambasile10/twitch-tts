ee3ac7a5862f3d5b22b7558401e3da1daf78ebfd
sambasile10 <sbasile2019@gmail.com>
2022-05-12 16:22:20 -0600

    Input Text Box

---
+++ src/components/InputTextBox.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="inputTextBox">
+        <textarea :model="message"
+                  placeholder="Input Text Box"
+                  rows="10"
+                  cols="40"></textarea>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'InputTextBox',
+        components: {},
+        props: {
+            inputText: String
+        }
+    }
+</script>
+
+<style scoped>
+    textarea {
+        border: 2px solid #888;
+        resize: none;
+    }
+</style>
